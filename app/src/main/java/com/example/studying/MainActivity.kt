241bf2afcbf4002d6a3fa94a4ff41b342338e1f8
anbv731 <14021165gh>
2022-02-17 11:03:57 +0400

    Initial commit

---
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.studying
+
+import Task1.books
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        books()
+        users()
+    }
+
+}
