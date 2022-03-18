f992201ffc48fbf47c974a2260fc28d916bf9b61
anbv731 <anbv731@yandex.ru>
2022-03-18 14:32:22 +0400

    task 7 to do new thread

--- app/src/main/java/com/example/studying/MainActivity.kt
+++ app/src/main/java/com/example/studying/MainActivity.kt
@@ -3,32 +3,64 @@ package com.example.studying
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    lateinit var recycler: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fun getJson(context: Context, fileName: String):String?{
-            val jsonString: String
-            try{
-                jsonString=context.assets.open(fileName).bufferedReader().use { it.readText() }
+        recycler = findViewById(R.id.recycler)
+        recycler.adapter = RecyclerAdapter(parseJson(getJson(this,"irlix.json")))
+        recycler.layoutManager = LinearLayoutManager(this)
 
-            }catch (ioException: IOException) {
+
+    }
+
+    fun parseJson(json:String?):List<Something> {
+
+        val gson = Gson()
+        val listSomthingType = object : TypeToken<List<Something>>() {}.type
+        return gson.fromJson(json, listSomthingType)
+
+
+    }
+    fun getJson(context: Context, fileName: String): String? {
+        val jsonString: String
+        try {
+            jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            return null
+        }
+        return jsonString
+
+    }
+    class secondThread:Thread(){
+        override fun run() {
+
+        }
+        fun parseJson(json:String?):List<Something> {
+
+            val gson = Gson()
+            val listSomthingType = object : TypeToken<List<Something>>() {}.type
+            return gson.fromJson(json, listSomthingType)
+        }
+        fun getJson(context: Context, fileName: String): String? {
+            val jsonString: String
+            try {
+                jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+            } catch (ioException: IOException) {
                 ioException.printStackTrace()
                 return null
             }
             return jsonString
 
         }
-        val json=getJson(this,"irlix.json")
-        val gson= Gson()
-
-        val listSomthingType = object : TypeToken<List<Something>>() {}.type
-        var somthings: List<Something> = gson.fromJson(json, listSomthingType)
-        somthings.forEachIndexed { idx, something -> println(something.id ) }
     }
-
 }
---
+++ app/src/main/java/com/example/studying/MyThread.kt
@@ -0,0 +1,7 @@
+package com.example.studying
+
+class MyThread :Thread (){
+    override fun run() {
+        super.run()
+    }
+}
