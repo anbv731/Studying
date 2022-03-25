8e27df27f179360e971f3a6158fd50fc33bdd89e
anbv731 <anbv731@yandex.ru>
2022-03-25 20:23:52 +0400

    task 9 to do SearchFragment

---
+++ app/src/main/java/com/example/studying/AuthFragment.kt
@@ -0,0 +1,69 @@
+package com.example.studying
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import com.example.studying.databinding.AuthBinding
+import com.jakewharton.rxbinding4.widget.afterTextChangeEvents
+import io.reactivex.rxjava3.core.Observable
+
+import io.reactivex.rxjava3.core.ObservableOnSubscribe
+import io.reactivex.rxjava3.core.Single
+import org.reactivestreams.Subscriber
+import java.util.*
+
+class AuthFragment : Fragment(R.layout.auth) {
+    lateinit var binding: AuthBinding
+    lateinit var editTextLogin: EditText
+    lateinit var editTextPassword: EditText
+    lateinit var button: Button
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = AuthBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        editTextLogin = binding.textEditLogin
+        editTextPassword = binding.textEditPassword
+        button = binding.buttonAuth
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener {
+            val searchFragment: Fragment = SearchFragment()
+            val fragmentTransaction = requireActivity()
+                .supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragmentContainer, searchFragment)
+            fragmentTransaction.commit()
+        }
+        startStream()
+
+    }
+
+   private fun startStream() {
+        val isLoginValid =
+            editTextLogin.afterTextChangeEvents().map { textview -> textview.view.text.length > 5 }
+                .distinctUntilChanged()
+        isLoginValid.map { a -> if (a) Color.BLACK else Color.RED }
+            .subscribe { c -> editTextLogin.setTextColor(c) }
+        val isPasswordValid = editTextPassword.afterTextChangeEvents()
+            .map { textview -> textview.view.text.length > 5 }.distinctUntilChanged()
+        isPasswordValid.map { a -> if (a) Color.BLACK else Color.RED }
+            .subscribe { c -> editTextPassword.setTextColor(c) }
+        val isSigneInPossible =
+            Observable.combineLatest(isLoginValid, isPasswordValid) { l, p -> l && p }
+                .distinctUntilChanged()
+        isSigneInPossible.subscribe { b -> button.isEnabled = b }
+        isSigneInPossible.subscribe { b -> button.isClickable = b }
+
+    }
+}
---
+++ app/src/main/java/com/example/studying/SearchFragment.kt
@@ -0,0 +1,109 @@
+package com.example.studying
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.studying.databinding.SearchBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableOnSubscribe
+import io.reactivex.rxjava3.core.ObservableSource
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.io.IOException
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class SearchFragment : Fragment(R.layout.search) {
+    lateinit var binding: SearchBinding
+    lateinit var searchView: SearchView
+    lateinit var textView: TextView
+    lateinit var recyclerView: RecyclerView
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = SearchBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        searchView = binding.searchView
+        textView = binding.textView
+        recyclerView = binding.recyclerView
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        //setList(parseJson(getJson(requireContext(), "irlix.json")))
+
+        Observable.create(ObservableOnSubscribe<String> { subscriber ->
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(text: String?): Boolean {
+                    subscriber.onNext(text)
+                    return false
+                }
+
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    subscriber.onNext(query)
+                    return false
+                }
+
+
+            })
+        }).map { text -> text.lowercase(Locale.getDefault()).trim() }
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .distinctUntilChanged()
+            .observeOn(AndroidSchedulers.mainThread())
+            //.observeOn(Schedulers.computation())
+            .subscribe { text ->
+                run {
+                    if (text.isEmpty()) {
+                        textView.visibility = View.VISIBLE
+                        recyclerView.visibility = View.GONE
+                    } else {
+                        textView.visibility = View.GONE
+                        recyclerView.visibility = View.VISIBLE
+                        val list = parseJson(getJson(requireContext(), "irlix.json"))
+                        setList(list.filter { text in it.title })
+                    }
+
+                }
+
+            }
+
+    }
+
+    private fun setList(list: List<Event>) {
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = RecyclerAdapter(list)
+    }
+}
+
+fun getJson(context: Context, fileName: String): String? {
+    val jsonString: String
+    try {
+        jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+    } catch (ioException: IOException) {
+        ioException.printStackTrace()
+        return null
+    }
+    return jsonString
+}
+
+fun parseJson(json: String?): List<Event> {
+
+    val gson = Gson()
+    val listEventType = object : TypeToken<List<Event>>() {}.type
+    return gson.fromJson(json, listEventType)
+}
